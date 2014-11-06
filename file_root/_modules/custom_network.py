dc6930521c0291531958587d0fe30f6a9ce678a6
akilesh1597 <akilesh1597@gmail.com>
2014-11-06 19:33:30 +0530

    network updates

--- file_root/_modules/custom_network.py
+++ file_root/_modules/custom_network.py
@@ -20,39 +20,8 @@ def linux_interfaces():
             close_fds=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT).communicate()[0]
-        cmd2 = subprocess.Popen(
-            '{0} addr show'.format(ip_path),
-            shell=True,
-            close_fds=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT).communicate()[0]
-        ifaces = _interfaces_ip(cmd1 + '\n' + cmd2)
+        ifaces = _interfaces_ip(cmd1)
     return ifaces
-
-
-
-def cidr_to_ipv4_netmask(cidr_bits):
-    '''
-    Returns an IPv4 netmask
-    '''
-    try:
-        cidr_bits = int(cidr_bits)
-        if not 1 <= cidr_bits <= 32:
-            return ''
-    except ValueError:
-        return ''
-
-    netmask = ''
-    for idx in range(4):
-        if idx:
-            netmask += '.'
-        if cidr_bits >= 8:
-            netmask += '255'
-            cidr_bits -= 8
-        else:
-            netmask += '{0:d}'.format(256 - (2 ** (8 - cidr_bits)))
-            cidr_bits = 0
-    return netmask
 
 def _interfaces_ip(out):
     '''
@@ -61,34 +30,12 @@ def _interfaces_ip(out):
     '''
     ret = dict()
 
-    def parse_network(value, cols):
-        '''
-        Return a tuple of ip, netmask, broadcast
-        based on the current set of cols
-        '''
-        brd = None
-        if '/' in value:  # we have a CIDR in this address
-            ip, cidr = value.split('/')  # pylint: disable=C0103
-        else:
-            ip = value  # pylint: disable=C0103
-            cidr = 32
-
-        if type_ == 'inet':
-            mask = cidr_to_ipv4_netmask(int(cidr))
-            if 'brd' in cols:
-                brd = cols[cols.index('brd') + 1]
-        elif type_ == 'inet6':
-            mask = cidr
-        return (ip, mask, brd)
-
     groups = re.compile('\r?\n\\d').split(out)
     for group in groups:
         iface = None
         data = dict()
 
         for line in group.splitlines():
-            if ' ' not in line:
-                continue
             match = re.match(r'^(\d*):\s+([\w.\-]+)(?:@)?([\w.\-]+)?:\s+<(.+)>', line)
             if match:
                 ifindex, iface, parent, attrs = match.groups()
@@ -101,44 +48,6 @@ def _interfaces_ip(out):
                 if ifindex:
                     data['index'] = ifindex
                 continue
-
-            cols = line.split()
-            if len(cols) >= 2:
-                type_, value = tuple(cols[0:2])
-                iflabel = cols[-1:][0]
-                if type_ in ('inet', 'inet6'):
-                    if 'secondary' not in cols:
-                        ipaddr, netmask, broadcast = parse_network(value, cols)
-                        if type_ == 'inet':
-                            if 'inet' not in data:
-                                data['inet'] = list()
-                            addr_obj = dict()
-                            addr_obj['address'] = ipaddr
-                            addr_obj['netmask'] = netmask
-                            addr_obj['broadcast'] = broadcast
-                            addr_obj['label'] = iflabel
-                            data['inet'].append(addr_obj)
-                        elif type_ == 'inet6':
-                            if 'inet6' not in data:
-                                data['inet6'] = list()
-                            addr_obj = dict()
-                            addr_obj['address'] = ipaddr
-                            addr_obj['prefixlen'] = netmask
-                            data['inet6'].append(addr_obj)
-                    else:
-                        if 'secondary' not in data:
-                            data['secondary'] = list()
-                        ip_, mask, brd = parse_network(value, cols)
-                        data['secondary'].append({
-                            'type': type_,
-                            'address': ip_,
-                            'netmask': mask,
-                            'broadcast': brd,
-                            'label': iflabel,
-                        })
-                        del ip_, mask, brd
-                elif type_.startswith('link'):
-                    data['hwaddr'] = value
         if iface:
             ret[iface] = data
             del iface, data
