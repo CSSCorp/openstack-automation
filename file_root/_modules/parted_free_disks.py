6b47b099cfec43ea455645b0be177a1431a787f9
akilesh1597 <akilesh1597@gmail.com>
2014-11-11 15:25:01 +0530

    max disk size in parted_free_disks

--- file_root/_modules/parted_free_disks.py
+++ file_root/_modules/parted_free_disks.py
@@ -5,7 +5,8 @@ __virtualname__ = 'partition_free_disks'
 def __virtual__():
     return __virtualname__
 
-def free_disks(unmounted_partitions=True, free_space=True, min_disk_size='10'):
+def free_disks(unmounted_partitions=True, free_space=True, min_disk_size='10',
+               max_disk_size=None):
     """
     Retrieve a list of disk devices that are not active
     do not include unmounted partitions if unmounted_partitions=False
@@ -21,10 +22,11 @@ def free_disks(unmounted_partitions=True, free_space=True, min_disk_size='10'):
     if unmounted_partitions:
         available_disks.extend(unmounted_partitions())
     if free_space:
-        for free_space in find_free_spaces(int(min_disk_size)):
+        while find_free_spaces(min_disk_size, max_disk_size):
             __salt__['partition.mkpart'](free_space['device'], 'primary',
                                          start=free_space['start'],
                                          end=free_space['end'])
+            __salt__['cmd.run']('partprobe')
             available_disks.append(free_space['device']+free_space['id'])
     return available_disks
 
@@ -71,7 +73,7 @@ def unmounted_partitions():
         
 
 
-def find_free_spaces(min_disk_size):
+def find_free_spaces(min_disk_size=10, max_disk_size=None):
     '''
     Retrieve a list of free space where partitions can be created
     returns device name, partition id when created, start and end sector
@@ -84,19 +86,24 @@ def find_free_spaces(min_disk_size):
 
         salt '*' partition.find_free_spaces
     '''
-    free_spaces = []
+    min_disk_size=int(min_disk_size)
     for block_device in get_block_device():
         device_name = '/dev/%s' % block_device
         part_data = __salt__['partition.part_list'](device_name, unit='s')
         sector_size = _sector_to_int(part_data['info']['logical sector'])
         disk_final_sector_int = _sector_to_int(part_data['info']['size'])
         last_device_id, last_allocated_sector_int = _last_allocated_sector(part_data['partitions'])
-        if ((disk_final_sector_int-last_allocated_sector_int)*sector_size)/1073741824 > min_disk_size:
-            free_spaces.append({'device': device_name,
-                                'id': str(last_device_id+1),
-                                'start': _int_to_sector(last_allocated_sector_int+1),
-                                'end': _int_to_sector(disk_final_sector_int-1)})
-    return free_spaces
+        disk_size_G = _sector_to_G(disk_final_sector_int-last_allocated_sector_int, sector_size)
+        if disk_size_G > min_disk_size:
+            start_sector_int = last_allocated_sector_int+1
+            if max_disk_size and disk_size_G > max_disk_size:
+                end_sector_int = start_sector_int + _G_to_sector(int(max_disk_size)) -1
+            else:
+                end_sector_int = disk_final_sector_int-1
+            return {'device': device_name,
+                    'id': str(last_device_id+1),
+                    'start': _int_to_sector(start_sector_int),
+                    'end': _int_to_sector(end_sector_int)}
 
 def _last_allocated_sector(part_data):
     last_allocated_sector = 2048
@@ -113,3 +120,9 @@ def _sector_to_int(sector):
 
 def _int_to_sector(int_sector):
     return str(int_sector) + 's'
+
+def _G_to_sector(giga_bytes, sector_size):
+    return (giga_bytes*1073741824)/sector_size
+
+def _sector_to_G(sector_length, sector_size):
+    return (sector_length*sector_size)/1073741824
