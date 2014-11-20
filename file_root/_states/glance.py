bf27f0768ecd5a3aa78238181eb4df616446f1d5
akilesh1597 <akilesh1597@gmail.com>
2014-11-20 11:43:47 +0530

    glance state fix

--- file_root/_states/glance.py
+++ file_root/_states/glance.py
@@ -91,7 +91,7 @@ def image_present(name,
     #iterate over all given arguments
     #if anything is different delete and recreate
     for key in non_null_arguments:
-        if existing_image.get(key, None) != non_null_arguments[key]:
+        if existing_image[name].get(key, None) != non_null_arguments[key]:
             LOG.debug('{0} has changed to {1}'.format(
                 key, non_null_arguments[key]))
             __salt__['glance.image_delete'](
