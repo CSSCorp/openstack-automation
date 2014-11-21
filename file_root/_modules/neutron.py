9c2052c969098c0cafebbf7e4f0ba043a515fc9f
akilesh1597 <akilesh1597@gmail.com>
2014-11-21 16:39:17 +0530

    neutron fix

--- file_root/_modules/neutron.py
+++ file_root/_modules/neutron.py
@@ -50,10 +50,18 @@ except ImportError:
     pass
 
 __opts__ = {}
+def __virtual__():
+    '''
+    Only load this module if glance
+    is installed on this minion.
+    '''
+    if HAS_NEUTRON:
+        return 'neutron'
+    return False
 
 
 def auth_decorator(func_name):
-    def decorator_method(_,
+    def decorator_method(
             profile=None, connection_user=None, connection_password=None,
             connection_tenant=None, connection_endpoint=None,
             conection_token=None, connection_auth_url=None, **kwargs):
@@ -65,132 +73,112 @@ def auth_decorator(func_name):
         endpoint = kstone.service_catalog.url_for(
             service_type='network',
             endpoint_type='publicURL')
-        neutron_interface = Neutron(client.Client(
-            endpoing_url=endpoint, token=token))
-        method_name = getattr(neutron_interface, func_name, None)
-        method_name(**kwargs)
+        neutron_interface = client.Client(
+            endpoing_url=endpoint, token=token)
+        return func_name(neutron_interface, **kwargs)
     return decorator_method
 
 
-class Neutron(object):
-    __name__ = 'neutron'
-    def __init__(self, client=None):
-        super(Neutron, self).__init__()
-        self._client = client
+@auth_decorator
+def list_floatingips(neutron_interface, **kwargs):
+    return neutron_interface.list_floatingips(**kwargs)['floatingips']
 
-    @auth_decorator
-    def list_floatingips(self, **kwargs):
-        return self.client.list_floatingips(**kwargs)['floatingips']
+def list_security_groups(neutron_interface, **kwargs):
+    return neutron_interface.list_security_groups(**kwargs)['security_groups']
 
-    def list_security_groups(self, **kwargs):
-        return self.client.list_security_groups(**kwargs)['security_groups']
+def list_subnets(neutron_interface, **kwargs):
+    return neutron_interface.list_subnets(neutron_interface, **kwargs)['subnets']
 
-    def list_subnets(self, **kwargs):
-        return self.client.list_subnets(self, **kwargs)['subnets']
+def list_networks(neutron_interface, **kwargs):
+    return neutron_interface.list_networks(neutron_interface, **kwargs)['networks']
 
-    def list_networks(self, **kwargs):
-        return self.client.list_networks(self, **kwargs)['networks']
+def list_ports(neutron_interface, **kwargs):
+    return neutron_interface.list_ports(neutron_interface, **kwargs)['ports']
 
-    def list_ports(self, **kwargs):
-        return self.client.list_ports(self, **kwargs)['ports']
+def list_routers(neutron_interface, **kwargs):
+    return neutron_interface.list_routers(neutron_interface, **kwargs)['routers']
 
-    def list_routers(self, **kwargs):
-        return self.client.list_routers(self, **kwargs)['routers']
+def update_security_group(neutron_interface, **kwargs):
+    neutron_interface.update_security_group({'security_group': kwargs})
 
-    def update_security_group(self, **kwargs):
-        self.client.update_security_group({'security_group': kwargs})
+def update_floating_ip(neutron_interface, fip, port_id):
+    neutron_interface.update_floatingip(fip, {"floatingip":
+                                        {"port_id": port_id}})
 
-    def update_floating_ip(self, fip, port_id):
-        self.client.update_floatingip(fip, {"floatingip":
-                                            {"port_id": port_id}})
+def update_subnet(neutron_interface, subnet_id, subnet):
+    neutron_interface.update_subnet(subnet_id, {'subnet': subnet})
 
-    def update_subnet(self, subnet_id, subnet):
-        self.client.update_subnet(subnet_id, {'subnet': subnet})
+def update_router(neutron_interface, router_id, external_network_id):
+    neutron_interface.update_router(router_id,
+                              {'router':
+                                  {'external_gateway_info':
+                                      {'network_id':
+                                          external_network_id}}})
 
-    def update_router(self, router_id, external_network_id):
-        self.client.update_router(router_id,
-                                  {'router':
-                                      {'external_gateway_info':
-                                          {'network_id':
-                                              external_network_id}}})
+def create_router(neutron_interface, **kwargs):
+    response = neutron_interface.create_router({'router': kwargs})
+    if 'router' in response and 'id' in response['router']:
+        return response['router']['id']
 
-    def create_router(self, **kwargs):
-        response = self.client.create_router({'router': kwargs})
-        if 'router' in response and 'id' in response['router']:
-            return response['router']['id']
+def router_add_interface(neutron_interface, router_id, subnet_id):
+    neutron_interface.add_interface_router(router_id, {'subnet_id': subnet_id})
 
-    def router_add_interface(self, router_id, subnet_id):
-        self.client.add_interface_router(router_id, {'subnet_id': subnet_id})
+def router_rem_interface(neutron_interface, router_id, subnet_id):
+    neutron_interface.remove_interface_router(router_id,
+                                        {'subnet_id': subnet_id})
 
-    def router_rem_interface(self, router_id, subnet_id):
-        self.client.remove_interface_router(router_id,
-                                            {'subnet_id': subnet_id})
+def create_security_group(neutron_interface, **kwargs):
+    response = neutron_interface.create_security_group({'security_group':
+                                                 kwargs})
+    if 'security_group' in response and 'id' in response['security_group']:
+        return response['security_group']['id']
 
-    def create_security_group(self, **kwargs):
-        response = self.client.create_security_group({'security_group':
-                                                     kwargs})
-        if 'security_group' in response and 'id' in response['security_group']:
-            return response['security_group']['id']
+def create_security_group_rule(neutron_interface, rule):
+    neutron_interface.create_security_group_rule({'security_group_rule':
+                                            rule})
 
-    def create_security_group_rule(self, rule):
-        self.client.create_security_group_rule({'security_group_rule':
-                                                rule})
+def create_floatingip(neutron_interface, floatingip):
+    response = neutron_interface.create_floatingip({'floatingip': floatingip})
+    if 'floatingip' in response and 'id' in response['floatingip']:
+        return response['floatingip']['id']
 
-    def create_floatingip(self, floatingip):
-        response = self.client.create_floatingip({'floatingip': floatingip})
-        if 'floatingip' in response and 'id' in response['floatingip']:
-            return response['floatingip']['id']
+def create_subnet(neutron_interface, **kwargs):
+    response = neutron_interface.create_subnet({'subnet': kwargs})
+    if 'subnet' in response and 'id' in response['subnet']:
+        return response['subnet']['id']
 
-    def create_subnet(self, **kwargs):
-        response = self.client.create_subnet({'subnet': kwargs})
-        if 'subnet' in response and 'id' in response['subnet']:
-            return response['subnet']['id']
+def create_network(neutron_interface, **kwargs):
+    response = neutron_interface.create_network({'network': kwargs})
+    if 'network' in response and 'id' in response['network']:
+        return response['network']['id']
 
-    def create_network(self, **kwargs):
-        response = self.client.create_network({'network': kwargs})
-        if 'network' in response and 'id' in response['network']:
-            return response['network']['id']
+def create_port(neutron_interface, **kwargs):
+    response = neutron_interface.create_port({'port': kwargs})
+    if 'port' in response and 'id' in response['port']:
+        return response['port']['id']
 
-    def create_port(self, **kwargs):
-        response = self.client.create_port({'port': kwargs})
-        if 'port' in response and 'id' in response['port']:
-            return response['port']['id']
+def update_port(neutron_interface, port_id, port):
+    neutron_interface.update_port(port_id, {'port': port})
 
-    def update_port(self, port_id, port):
-        self.client.update_port(port_id, {'port': port})
+def delete_floating_ip(neutron_interface, floating_ip_id):
+    neutron_interface.delete_floatingip(floating_ip_id)
 
-    def delete_floating_ip(self, floating_ip_id):
-        self.client.delete_floatingip(floating_ip_id)
+def delete_security_group(neutron_interface, sg_id):
+    neutron_interface.delete_security_group(sg_id)
 
-    def delete_security_group(self, sg_id):
-        self.client.delete_security_group(sg_id)
+def delete_security_group_rule(neutron_interface, rule):
+    sg_rules = neutron_interface.list_security_group_rules(
+        security_group_id=rule['security_group_id'])
+    for sg_rule in sg_rules['security_group_rules']:
+        sgr_id = sg_rule.pop('id')
+        if sg_rule == rule:
+            neutron_interface.delete_security_group_rule(sgr_id)
 
-    def delete_security_group_rule(self, rule):
-        sg_rules = self.client.list_security_group_rules(
-            security_group_id=rule['security_group_id'])
-        for sg_rule in sg_rules['security_group_rules']:
-            sgr_id = sg_rule.pop('id')
-            if sg_rule == rule:
-                self.client.delete_security_group_rule(sgr_id)
+def delete_subnet(neutron_interface, subnet_id):
+    neutron_interface.delete_subnet(subnet_id)
 
-    def delete_subnet(self, subnet_id):
-        self.client.delete_subnet(subnet_id)
+def delete_network(neutron_interface, network_id):
+    neutron_interface.delete_network(network_id)
 
-    def delete_network(self, network_id):
-        self.client.delete_network(network_id)
-
-    def delete_router(self, router_id):
-        self.client.delete_router(router_id)
-
-    @staticmethod
-    def __virtual__():
-        '''
-        Only load this module if glance
-        is installed on this minion.
-        '''
-        if HAS_NEUTRON:
-            return 'neutron'
-        return False
-
-
-sys.modules[__name__] = Neutron()
+def delete_router(neutron_interface, router_id):
+    neutron_interface.delete_router(router_id)
